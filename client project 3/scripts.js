9f4a1ccaa7e565386416fe4fc78ae1ce85323043
maybesab <maysab@umich.edu>
2025-04-07 15:14:55 -0400

    Delete client project 3 directory

--- client project 3/scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', () => {
-    const yearSpan = document.getElementById('current-year');
-    if (yearSpan) {
-        const thisYear = new Date().getFullYear();
-        yearSpan.textContent = thisYear;
-    }
-});
